e0bddb6e4cdb819cbe19584dbc5a72b8b6a22062
gabriel <vnvsgabe@gmail.com>
2024-01-19 14:35:03 -0300

    melhroado index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,12 @@ import styled from 'styled-components'
 import BackgroundImageWithChildren from '../src/components/layout/background-image-with-children/BackgroundImageWithChildren'
 
 const StyledDiv = styled.div`
-  text-align: center;
-  font-size: 28px;
-  background-color: bisque;
+  background-color: ${(props) => props.theme.colors.white};
+  width: 650px;
+
+  @media (max-width: 650px) {
+    min-width: 100%;
+  }
 `
 
 export default function homePage() {
