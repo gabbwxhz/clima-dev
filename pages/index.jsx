1f2966c964a7bd5ee7d460417159a4981fb8176a
gabriel <vnvsgabe@gmail.com>
2024-01-22 18:58:04 -0300

    criado Logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
 import BackgroundImageWithChildren from '../src/components/layout/background-image-with-children/BackgroundImageWithChildren'
+import Logo from '../src/components/logo/Logo'
 
 const StyledDiv = styled.div`
   background-color: ${(props) => props.theme.colors.white};
   width: 650px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 40px;
 
   @media (max-width: 650px) {
     min-width: 100%;
@@ -14,7 +19,10 @@ const StyledDiv = styled.div`
 export default function homePage() {
   return (
     <BackgroundImageWithChildren>
-      <StyledDiv>HomePage</StyledDiv>
+      <StyledDiv>
+        <Logo showImage type='vertical' />
+        <input />
+      </StyledDiv>
     </BackgroundImageWithChildren>
   )
 }
---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+const Img = styled.img``
+
+const VerticalText = styled.h1`
+  font-size: 80px;
+  color: ${(props) => props.theme.colors.black};
+  line-height: 70px;
+  margin-left: 150px;
+  margin-top: -70px;
+`
+
+const PrimaryColor = styled.span`
+  color: ${(props) => props.theme.colors.primary};
+`
+
+export default function Logo({ showImage, type }) {
+  return (
+    <div>
+      {showImage && <Img src="/woman.svg" />}
+      {type === 'vertical' && (
+        <VerticalText>
+          Clima <br />
+          <PrimaryColor>DEV</PrimaryColor>
+        </VerticalText>
+      )}
+    </div>
+  )
+}
