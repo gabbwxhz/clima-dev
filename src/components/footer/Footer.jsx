95300b0572d81f5060098dea2e1a68a60fdcb425
gabriel <vnvsgabe@gmail.com>
2024-02-19 19:33:29 -0300

    adicionado ponto final na frase do footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,17 @@
 import styled from 'styled-components'
 
-import Container from '../layout/container/Container'
-
 const StyledFooter = styled.div`
   background-color: ${(props) => props.theme.colors.white};
   text-align: center;
   padding: 20px 0;
   font-weight: 400;
   font-size: 20px;
-  top: 20px;
 `
 
 export default function Footer() {
   return (
     <StyledFooter>
-      Criado por <b> Gabriel Picoli</b>, 2024
+      Criado por <b> Gabriel Picoli</b>, 2024.
     </StyledFooter>
   )
 }
