46e9f8ed5067e692486f75ca0b58daf39cc96038
gabriel <vnvsgabe@gmail.com>
2024-02-21 19:17:46 -0300

    modificado a responsividade

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
-const Img = styled.img``
+const Img = styled.img`
+  @media (max-width: 320px) {
+    display: none;
+  }
+`
 
 const VerticalText = styled.h1`
   font-size: 80px;
@@ -9,6 +13,17 @@ const VerticalText = styled.h1`
   margin-left: 150px;
   margin-top: -70px;
   margin-bottom: 50px;
+
+  @media (max-width: 425px) {
+    margin-left: 130px;
+  }
+  @media (max-width: 375px) {
+    font-size: 60px;
+  }
+  @media (max-width: 320px) {
+    margin: 0 auto;
+    margin-bottom: 20px;
+  }
 `
 
 const HorizontalText = styled.h1`
